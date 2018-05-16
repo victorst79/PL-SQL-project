3b96f180fecbf94b16b2d1dbfc00e2210ed53fb3
Victor Santaella <victorst79@gmail.com>
2018-05-16 12:38:40 +0200

    PL/SQL functions

--- Library.sql
+++ Library.sql
@@ -12,7 +12,7 @@ CREATE TABLE Customer(
   name VARCHAR2(40),
   customerAddress VARCHAR2(50),
   phone NUMBER(9),
-  password VARCHAR(20),
+  password VARCHAR2(20),
   userName VARCHAR2(10),
   dateSignUp DATE,
   cardNumber NUMBER,
@@ -23,7 +23,7 @@ CREATE TABLE Employee(
   name VARCHAR2(40),
   employeeAddress VARCHAR2(50),
   phone NUMBER(9),
-  password VARCHAR(20),
+  password VARCHAR2(20),
   userName VARCHAR2(10),
   paycheck NUMBER (8, 2),
   branchName VARCHAR2(40),
@@ -32,7 +32,7 @@ CREATE TABLE Employee(
 
 CREATE TABLE Branch(
   name VARCHAR2(40),
-  address VARCHAR(50),
+  address VARCHAR2(50),
   phone NUMBER(9),
   CONSTRAINT Branch_PK PRIMARY KEY (name));
   
@@ -42,25 +42,25 @@ CREATE TABLE Location(
   
 CREATE TABLE Rent(
   cardID NUMBER,
-  itemID VARCHAR(6),
+  itemID VARCHAR2(6),
   apporpriationDate DATE,
   returnDate DATE,
   CONSTRAINT Rent_PK PRIMARY KEY (cardID,itemID));
  
 CREATE TABLE Book(
-  ISBN VARCHAR(4),
-  bookID VARCHAR(6),
+  ISBN VARCHAR2(4),
+  bookID VARCHAR2(6),
   state VARCHAR2(10),
   avalability VARCHAR2(1) CHECK ((avalability = 'A') OR (avalability = 'O')),
   debyCost NUMBER(10,2),
   lostCost NUMBER(10,2),
-  address VARCHAR(50),
+  address VARCHAR2(50),
   CONSTRAINT Book_PK PRIMARY KEY (ISBN,bookID));
 
 CREATE TABLE Video(
-  title VARCHAR(50),
+  title VARCHAR2(50),
   year INT,
-  videoID VARCHAR(6),
+  videoID VARCHAR2(6),
   state VARCHAR2(10),
   avalability VARCHAR2(1) CHECK ((avalability = 'A') OR (avalability = 'O')),
   debyCost NUMBER(10,2),
@@ -203,3 +203,289 @@ INSERT INTO Rent VALUES (104, 'V1JA15', '01-05-2018', '21-05-2018');
 INSERT INTO Rent VALUES (105, 'V1DI00', '02-05-2018', '25-05-2018');
 INSERT INTO Rent VALUES (154, 'B1L321', '04-05-2018', '26-05-2018');
 INSERT INTO Rent VALUES (155, 'V1CH16', '29-04-2018', '29-05-2018');
+
+--FUNCTIONS--
+--1--
+CREATE OR REPLACE PROCEDURE login_library(user IN VARCHAR2, pass IN VARCHAR2)
+IS
+  passAux customer.password%TYPE;
+BEGIN
+  SELECT password INTO passAux
+  FROM customer
+  WHERE username LIKE user;
+  
+  IF passAux LIKE pass THEN
+    DBMS_OUTPUT.PUT_LINE('User ' || user || ' loging succesfull');
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('Password incorrect');
+  END IF;
+  
+  EXCEPTION WHEN no_data_found THEN 
+  DBMS_OUTPUT.PUT_LINE('User incorrect');
+END;
+
+SET SERVEROUTPUT ON;
+DECLARE
+  user customer.username%TYPE;
+  pass customer.password%TYPE;
+BEGIN
+  user := &Username;
+  pass := &Password;
+  login_library(user,pass);
+END;
+--2--
+
+
+--3--
+
+CREATE OR REPLACE PROCEDURE customerAcount_library(custoID IN customer.customerid%TYPE)
+IS
+  auxCard NUMBER;
+  auxFines NUMBER;
+  auxItem VARCHAR(6);
+  rented number := 0;
+BEGIN
+  SELECT cardnumber INTO auxCard
+  FROM customer
+  WHERE customerid LIKE custoID;
+  
+  SELECT COUNT(*) INTO rented
+  FROM rent
+  WHERE rent.cardid LIKE auxcard;
+  
+  DBMS_OUTPUT.PUT_LINE('The user card is ' || auxCard);  
+  IF (rented > 0) THEN
+    SELECT rent.itemid INTO auxItem
+    FROM rent,card
+    WHERE card.cardid = rent.cardid
+    AND card.cardid LIKE auxCard;    
+    
+    DBMS_OUTPUT.PUT_LINE('The user has ' || auxItem || ' rented');
+  ELSE    
+    DBMS_OUTPUT.PUT_LINE('This user has no rents'); 
+  END IF;
+  
+  SELECT fines INTO auxFines
+  FROM card
+  WHERE cardid LIKE auxcard;
+  
+  DBMS_OUTPUT.PUT_LINE('The user fines are ' || auxFines);
+    
+  EXCEPTION WHEN no_data_found THEN 
+  DBMS_OUTPUT.PUT_LINE('NOT DATA FOUND');
+END;
+
+SET SERVEROUTPUT ON;
+DECLARE
+  custoID customer.customerid%TYPE;
+BEGIN
+  custoID := &Customer_ID;
+  customerAcount_library(custoID);
+END;
+
+--4--
+CREATE OR REPLACE PROCEDURE rentItem_library(auxCard IN card.cardid%TYPE, auxItemID IN VARCHAR2, itemType IN VARCHAR2, auxDate IN DATE)
+IS
+BEGIN
+  
+  IF itemType LIKE 'book' THEN
+    UPDATE book
+    SET avalability = 'O'
+    WHERE bookid LIKE auxItemID;
+    
+  ELSIF itemType LIKE 'video' THEN
+    UPDATE video
+    SET avalability = 'O'
+    WHERE videoid LIKE auxItemID;
+  END IF;
+    
+    INSERT INTO rent
+    VALUES (auxCard,auxItemID,sysdate,auxDate);
+    DBMS_OUTPUT.PUT_LINE('ITEM ' || auxItemID || ' RENTED');
+END;
+
+SET SERVEROUTPUT ON;
+DECLARE
+  auxCard card.cardid%TYPE;
+  auxItemID VARCHAR2(10);
+  itemType VARCHAR2(20);
+  auxDate DATE;
+BEGIN
+  auxCard := &Card_ID;
+  auxItemID := &ID_Item;
+  itemType := &Item_Type_(book_or_video);
+  auxDate := &Return_date;
+  rentItem_library(auxCard,auxItemID,itemType,auxDate);
+END;
+--5--
+
+CREATE OR REPLACE PROCEDURE payFines_library(auxCard IN card.cardid%TYPE, money IN NUMBER)
+IS
+  finesAmount NUMBER;
+  total NUMBER;
+BEGIN
+  SELECT fines INTO finesAmount
+  FROM card
+  WHERE cardid LIKE auxCard;
+  
+  IF finesAmount < money THEN
+    total := money - finesAmount;
+    DBMS_OUTPUT.PUT_LINE('YOU PAY ALL YOUR FINES AND YOU HAVE ' || total || ' MONEY BACK');
+    
+    UPDATE card
+    SET status = 'A', fines = 0
+    WHERE cardid = auxCard;
+    
+  ELSIF finesAmount = money THEN
+    total := money - finesAmount;
+    DBMS_OUTPUT.PUT_LINE('YOU PAY ALL YOUR FINES');
+    
+    UPDATE card
+    SET status = 'A', fines = 0
+    WHERE cardid = auxCard;
+  
+  ELSE
+    total := finesAmount - money;
+    DBMS_OUTPUT.PUT_LINE('YOU WILL NEED TO PAY ' || total || ' MORE DOLLARS TO UNLOCK YOUR CARD');
+    
+    UPDATE card
+    SET fines = total
+    WHERE cardid = auxCard;
+  END IF;
+END;
+
+SET SERVEROUTPUT ON;
+DECLARE
+  auxCard card.cardid%TYPE;
+  money NUMBER;
+BEGIN
+  auxCard := &Card_ID;
+  money := &Money_To_Pay;
+  payFines_library(custoID);
+END;
+
+--6--
+CREATE OR REPLACE PROCEDURE updateInfo_library(auxCustomer IN customer.customerid%TYPE, pNumber NUMBER, address VARCHAR2, newPass VARCHAR2)
+IS
+BEGIN
+  UPDATE customer
+  SET phone = pNumber, customeraddress = address, password = newPass
+  WHERE customerid = auxCustomer;
+END;
+
+SET SERVEROUTPUT ON;
+DECLARE
+  auxCustomer customer.customerid%TYPE;
+  pNumber NUMBER;
+  address VARCHAR2;
+  newPass VARCHAR2;
+BEGIN
+  auxCustomer := &Customer_ID;
+  pNumber := &Write_your_new_phone_number_or_the_old_one_if_you_do_not_want_to_change_it;
+  address := &Write_your_new_address_or_the_old_one_if_you_do_not_want_to_change_it;
+  newPass := &Write_your_new_password_or_the_old_one_if_you_do_not_want_to_change_it;
+  updateInfo_library(auxCustomer,pNumber,address,newPass);
+END;
+
+--7--
+CREATE OR REPLACE PROCEDURE addCustomer_library(auxCustomerId IN NUMBER, auxName IN VARCHAR2, auxCustomerAddress IN VARCHAR2, auxPhone IN NUMBER,
+auxPass IN VARCHAR2, auxUserName IN VARCHAR2, auxCardNumber IN NUMBER)
+IS
+BEGIN
+  INSERT INTO customer
+  VALUES (auxCustomerId,auxName,auxCustomerAddress,auxPhone,auxPass,auxUserName,sysdate,auxCardNumber);
+END;
+
+SET SERVEROUTPUT ON;
+DECLARE
+  auxCustomerId NUMBER;
+  auxName VARCHAR2(20);
+  auxCustomerAddress VARCHAR2(20);
+  auxPhone NUMBER;
+  auxPass VARCHAR2(20);
+  auxUserName VARCHAR2(20);
+  auxCardNumber NUMBER;
+BEGIN
+  auxCustomerId := &Customer_ID;
+  auxName := &Name;
+  auxCustomerAddress := &Address;
+  auxPhone := &Phone;
+  auxPass := &Password;
+  auxUserName := &User_Name;
+  auxCardNumber := &Card_Numeber;
+  addCustomer_library(auxCustomerId,auxName,auxCustomerAddress,auxPhone,auxPass,auxUserName,auxCardNumber);
+END;
+
+--8--
+CREATE OR REPLACE TRIGGER addCardTrigger_library
+AFTER INSERT
+ON customer
+FOR EACH ROW
+DECLARE
+BEGIN
+  INSERT INTO card
+  VALUES (:new.cardnumber,'A',0);
+  
+  DBMS_OUTPUT.PUT_LINE('Card created');
+END;
+
+--EXAMPLE--
+INSERT INTO customer
+VALUES (11,'MARI CARMEN','CORDOBA',645892456,'maricarmen123','ma11',sysdate,111);
+
+--9--
+CREATE OR REPLACE PROCEDURE allMedia_library(mediaType VARCHAR2)
+IS
+  CURSOR cBooks
+  IS
+    SELECT *
+    FROM book;
+  
+  CURSOR cVideos
+  IS
+    SELECT *
+    FROM video;
+  
+  xBooks cBooks%ROWTYPE;
+  xVideos cVideos%ROWTYPE;
+BEGIN
+  IF mediaType LIKE 'books' THEN
+    OPEN cBooks;
+    
+    DBMS_OUTPUT.PUT_LINE('ISBN     ID     STATE     AVALABILITY     DEBY_COST     LOST_COST    LOCATION');
+    DBMS_OUTPUT.PUT_LINE('-----------------------------------------------------------------------------');
+    
+    LOOP
+      FETCH cBooks
+      INTO xBooks;
+      EXIT WHEN cBooks%NOTFOUND;
+      
+      DBMS_OUTPUT.PUT_LINE(xBooks.isbn || '     ' || xBooks.bookid || '     ' || xBooks.state || '     ' || xBooks.avalability || '     ' || xBooks.debycost || '     ' ||
+      xBooks.lostcost || '     ' || xBooks.address);
+    END LOOP;
+  ELSIF mediaType LIKE 'videos' THEN
+    OPEN cVideos;
+    DBMS_OUTPUT.PUT_LINE('TITLE     YEAR     ID     STATE     AVALABILITY     DEBY_COST     LOST_COST    LOCATION');
+    DBMS_OUTPUT.PUT_LINE('---------------------------------------------------------------------------------------');
+    LOOP
+      FETCH cVideos
+      INTO xVideos;
+      EXIT WHEN cVideos%NOTFOUND;
+      
+      DBMS_OUTPUT.PUT_LINE(xVideos.title || '     ' || xVideos.year || '     ' || xVideos.videoid || '     ' || xVideos.state || '     ' || xVideos.avalability || '     ' || xVideos.debycost || '     ' ||
+      xVideos.lostcost || '     ' || xVideos.address);
+    END LOOP;
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('TYPE INCORRECT, you must choose between books or videos');
+  END IF;
+END;
+
+SET SERVEROUTPUT ON;
+DECLARE
+  typeItem VARCHAR2(10);
+BEGIN
+  typeItem := &Select_between_books_or_videos;
+  allMedia_library(typeItem);
+END;
+
+--10--
